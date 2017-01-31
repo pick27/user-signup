258cd8764b318cf11783fcb657d0d0ee136823fa
Scott Land <101.scott@gmail.com>
2017-01-30 20:20:07 -0600

    added a few more bad input options

--- main.py
+++ main.py
@@ -63,14 +63,18 @@ class MainHandler(webapp2.RequestHandler):
         password = self.request.get("password")
         verify = self.request.get("verify")
         email = self.request.get("email")
+###
         username_error = ""
         user_re = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
         if username == "":
             username_error = "Username is a required field."
         elif " " in username:
             username_error = "Username may not contain a space."
+        elif len(username) < 3 or len(username) > 20:
+            username_error = "Username is 3 to 20 characters long."
         elif not user_re.match(username):
             username_error = "That's not a valid username."
+###
         password_error = ""
         password_re = re.compile(r"^.{3,20}$")
         if password != verify:
@@ -81,15 +85,19 @@ class MainHandler(webapp2.RequestHandler):
             password_error = "Password may not contain a space."
         elif not password_re.match(password):
             password_error = "That wasn't a valid password"
+###
         email_error = ""
         email_re = re.compile(r"^[\S]+@[\S]+.[\S]+$")
         email = email.strip()
         if " " in email:
             email_error = "Email may not contain a space."
+        elif "," in email or ";" in email or "<" in email or ">" in email:
+            email_error = "That's not a valid email address."
         elif email != "" and not email_re.match(email):
             email_error = "That's not a valid email address."
         if password_error + username_error + email_error == "":
             self.redirect("/welcome?username=%s" % username)
+###
         content = buildform(username, email, username_error, password_error, email_error)
         self.response.write(content)
 
